30cc7f6bc6e6365a5eb98de61916ea58f53a4e9e
Bijoy Paul <kgec.bijoypaul@gmail.com>
2017-01-23 02:10:26 +0530

    unused annotation removed

--- src/main/java/org/basic/spring/security/rest/config/ApiSecurityConfig.java
+++ src/main/java/org/basic/spring/security/rest/config/ApiSecurityConfig.java
@@ -1,7 +1,6 @@
 package org.basic.spring.security.rest.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,7 +18,6 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationEn
  *
  * @author Bijoy Paul
  */
-@Configuration
 @EnableWebSecurity
 public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
 
